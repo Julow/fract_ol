b3a79d23473a1fd2074e67a401b468c3af02a127
jaguillo <juloo.dsi@gmail.com>
2014-12-17 18:58:14 +0100

    Squashed 'libft/' changes from 2a009b1..ed2ef69
    ed2ef69 t_big + draw transparency
    ab69279 Fix segfault
    9bac1af Optimise get_next_line and fix bug with folder
    a1bb058 ft_hidenp
    
    git-subtree-dir: libft
    git-subtree-split: ed2ef6994f4a214c659efd2a070c322441ec2fcd

--- srcs/ft_hidenp.c
+++ srcs/ft_hidenp.c
@@ -1,31 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_drawpt.c                                        :+:      :+:    :+:   */
+/*   ft_hidenp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/12/11 15:51:49 by jaguillo          #+#    #+#             */
-/*   Updated: 2014/12/11 15:51:49 by jaguillo         ###   ########.fr       */
+/*   Created: 2014/12/16 22:55:17 by jaguillo          #+#    #+#             */
+/*   Updated: 2014/12/16 22:55:17 by jaguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void			ft_drawpt(t_image *img, t_pt pt, t_color color)
+t_bool			ft_hidenp(char *hide, char *str)
 {
-	int				pos;
-	int				i;
-
-	if (pt.x < 0 || pt.x >= img->width || pt.y < 0 || pt.y >= img->height)
-		return ;
-	if (ALPHA(color))
-		ft_resalpha(&color, ft_imagept(img, pt));
-	pos = (img->width * pt.y + pt.x) * img->opp;
-	i = -1;
-	while (++i < img->opp)
-	{
-		img->data[pos + i] = color.b.b;
-		color.i = color.i >> 8;
-	}
+	while (*(str++) != '\0')
+		if (*hide == *str)
+			hide++;
+	return ((*hide == '\0') ? TRUE : FALSE);
 }
