0f14f3f68ca0b5893ce91cf6d440eb46359ede6c
juloo <juloo.dsi@gmail.com>
2015-01-03 03:30:10 +0100

    Adapt to libft

--- srcs/ft_imagenew.c
+++ srcs/ft_imagenew.c
@@ -6,21 +6,24 @@
 /*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/11 16:48:16 by jaguillo          #+#    #+#             */
-/*   Updated: 2014/12/11 16:48:17 by jaguillo         ###   ########.fr       */
+/*   Updated: 2015/01/03 02:29:39 by jaguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 #include <mlx.h>
+#include <stdlib.h>
 
 t_image			*ft_imagenew(void *mlx, t_pt size)
 {
 	t_image			*img;
 	int				bpp;
 
-	img = MAL1(t_image);
+	if ((img = MAL1(t_image)) == NULL)
+		return (NULL);
 	img->img = mlx_new_image(mlx, size.x, size.y);
-	img->data = mlx_get_data_addr(img->img, &bpp, &img->l_size, &img->endian);
+	img->data = (t_byte*)mlx_get_data_addr(img->img, &bpp, &img->l_size,
+		&img->endian);
 	img->width = size.x;
 	img->height = size.y;
 	img->opp = bpp / 8;
